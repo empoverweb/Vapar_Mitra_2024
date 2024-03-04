8d1ea279ab0238769beb86a6505c090ec77f5dee
gurram-saikumar <saikumar.gurram@gmail.com>
2024-03-04 11:54:57 +0530

    added the primereact datables config

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -2,4 +2,4 @@ import { CURRENT_ENV, CONFIG } from '@/configs/config';
 
 const  BASE_URL = CONFIG[CURRENT_ENV].BASE_URL;
    
-export const HybridGetData = `${BASE_URL}unauthorised/gridData?actionType=hybridInfoService&extraParams=CP&start=1&limit=10`;
+export const addDistrict = `${BASE_URL}VyaparMitra/rest/nsl/addDistricts`;
--- src/widgets/primedatatable/index.jsx
+++ src/widgets/primedatatable/index.jsx
@@ -14,6 +14,7 @@ import { InputNumber } from 'primereact/inputnumber';
  import { InputText } from 'primereact/inputtext';
 import { Tag } from 'primereact/tag'; 
 import Modal from '../modal';
+import { TrashIcon } from '@heroicons/react/24/solid';
 
 export default function PrimeDataTable() {
     let emptyProduct = {
@@ -178,10 +179,25 @@ export default function PrimeDataTable() {
 
     const leftToolbarTemplate = () => {
         return (
-            <div className="flex flex-wrap gap-2">
-                <Button label="New" icon="pi pi-plus" severity="success" onClick={openNew} />
-                <Button label="Delete" icon="pi pi-trash" severity="danger" onClick={confirmDeleteSelected} disabled={!selectedProducts || !selectedProducts.length} />
-            </div>
+            <div className="flex flex-wrap gap-2"> 
+               <button
+                class="flex select-none items-center gap-1 rounded-lg bg-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button" onClick={openNew} >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
+                  stroke-width="2" class="w-4 h-4">
+                  <path
+                    d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z">
+                  </path>
+                </svg>
+                Add User
+              </button>   
+              <button
+                class="flex select-none items-center gap-1 rounded-lg bg-red-500 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button" onClick={confirmDeleteSelected} disabled={!selectedProducts || !selectedProducts.length} >
+                 <TrashIcon className='w-4 h-4'/>
+               Delete
+              </button>  
+             </div>
         );
     };
 
