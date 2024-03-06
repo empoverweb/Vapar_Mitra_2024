90ea10efd6dd297d76006f7d7afc6c9f637f4cf2
ravikumar.m <ravikumar.medi@empover.com>
2024-03-06 19:12:50 +0530

    added the state master

--- src/pages/masters/index.js
+++ src/pages/masters/index.js
@@ -10,6 +10,7 @@ export * from "@/pages/masters/addproductionplants";
 export * from "@/pages/masters/addSeasons"; 
 export * from "@/pages/masters/addHeadQuarters"; 
 export * from "@/pages/masters/AddCountry"; 
+export * from "@/pages/masters/AddState"; 
 
 
 
