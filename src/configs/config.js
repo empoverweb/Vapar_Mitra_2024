5b91b34bef69c050e6e9f141c65ea3bf95740fbe
empoverweb <162984502+empoverweb@users.noreply.github.com>
2024-03-12 12:10:00 +0530

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,7 +2,7 @@ export const CURRENT_ENV = "DEV";
 
 export const CONFIG = {
     DEV: {
-        BASE_URL: "http://123.176.45.59:8080/VyaparMitra/rest/nsl/"
+        BASE_URL: "http://10.20.0.155:8080//VyaparMitra/rest/nsl/"
     },
     PROD:{
         BASE_URL: "https://prod.pioneeractivity.in/IMS/"
